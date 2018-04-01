c0aae21f915ce34bf38c5760d287d382a8061574
jasonLiu001 <wangliu@isoftstone.com>
2018-04-01 11:24:50 +0800

    tomcat热部署

--- shell/deploy_site.sh
+++ shell/deploy_site.sh
@@ -3,11 +3,7 @@
 cd /home/github/simple-blog;
 # 编译项目
 mvn package;
-# 停止tomcat服务
-sh /home/apache-tomcat-9.0.6/bin/shutdown.sh;
 # 删除simplebolg目录
 rm -rf /home/apache-tomcat-9.0.6/webapps/simpleblog/;
 # 拷贝simpleblog.war到tomcat目录
 cp -rf /home/github/simple-blog/target/simpleblog.war /home/apache-tomcat-9.0.6/webapps/;
-# 启动tomcat服务
-sh /home/apache-tomcat-9.0.6/bin/startup.sh;
